6740353cbd793c9a3528919e0338753e5a1e8fba
Szymon <seltinxx@gmail.com>
2021-04-25 22:01:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+# This is import numpy as np
+# # Zadanie 1
+# # a = np.arange(3)
+# # print(a)
+# # b = np.arange(3)
+# # print(b)
+# # c = a * b
+# # print(c)
+#
+# #Zadanie 2
+#
+# # a = np.arange(9).reshape(3,3)
+# # print(a)
+# # print(a.min(axis=1))
+# # b = np.arange(16).reshape(4,4)
+# # print(b)
+# # print(b.min(axis=1))
+#
+# #Zadanie 3
+#
+# # print(a.dot(b))
+#
+# #Zadanie 4
+#
+# # a = np.arange(3).reshape(1,3)
+# # print(a)
+# # b = np.array([1,1/2,-3])
+# # print(b)
+# # c = a * b
+# # print(c)
+#
+# #Zadanie 5
+#
+# # b = np.arange(6).reshape(2,3)
+# # a = np.sin(b)
+# # print(a)
+#
+# #Zadanie 6
+#
+# # c = np.arange(6).reshape(2,3)
+# # b = np.cos(c)
+# # print(b)
+#
+# #Zadanie 7
+#
+# # x = a + b
+# # print(x)
+#
+# #Zadanie 8
+# # a = np.arange(9).reshape(3,3)
+# # # print(a)
+# # for b in a:
+# #     print(b)
+# #     print("")
+#
+# #Zadanie 9
+# # a = np.arange(9).reshape(3,3)
+# # for b in a.flat:
+# #     print(b)
+# #     print("")
+#
+# #Zadanie 10
+# # a = np.arange(81).reshape(9,9)
+# # print(a.shape)
+#
+# #Zadanie 11
